dc6e78e5adb04bd5db1ddce66feab35516af85d2
kazikomol <kazikomol2@gmail.com>
2026-04-09 21:40:11 +0600

    first commit

--- script/login.js
+++ script/login.js
@@ -1,22 +1,22 @@
 document.getElementById('login-btn').addEventListener('click',function(event){
     event.preventDefault();
-    //1.get the mobile number
+   
     const usernameInput = document.getElementById("username");
     const username = usernameInput.value;
    
-    //2.get the pin
+    
     const pinInput = document.getElementById("password");
     const pin = pinInput.value;
     
-    //3.match the pin & mobile number
+    
     if(username=='admin' && pin=='admin123')
     {
-        //true: alert
+       
         alert('login successfull');
         window.location.replace('/home.html')
     }
     else {
-        //false:alert
+        
         alert("login unseccessfull");
         return;
 }
